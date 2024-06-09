06e07c5574c45fc7af25357a502c65a46412b9f5
Marco Scata <mscata@hotmail.com>
2024-06-09 21:24:57 +0100

    Externalising artifact version

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
+
+with open('version.txt') as version_file:
+    version = version_file.read().strip()
+
 setup(
     name='cicdlab-flask-runner',
     packages=['cicdlab-flask-runner'],
-    version='0.0.0',
+    version=version,
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='A simple runner for Flask apps',
     author='Marco Scata',
@@ -10,9 +14,6 @@ setup(
     url='http://scmserver:3000/mscata/cicdlab-flask-runner',
     download_url='https://github.com/mscata/cicdlab-flask-runner/archive/v_01.tar.gz',
     keywords=['CI/CD'],
-    install_requires=[
-        'flask',
-    ],
     classifiers=[ # Pick from https://pypi.org/classifiers/
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
